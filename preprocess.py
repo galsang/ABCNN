a540ecf915289fca5db4175af518d6294b1aa3ea
galsang <kimtaeuk1000@naver.com>
2017-03-22 22:13:23 +0900

    modified

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,15 @@
-
+import tensorflow as tf
 import numpy as np
 import nltk
 import gensim
 
+
 class Word2Vec():
     def __init__(self):
         # Load Google's pre-trained Word2Vec model.
-        self.model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
-        self.unknowns = np.random.uniform(-0.01,0.01,300).astype("float32")
+        self.model = gensim.models.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin',
+                                                                     binary=True)
+        self.unknowns = np.random.uniform(-0.01, 0.01, 300).astype("float32")
 
     def get(self, word):
         if word not in self.model.vocab:
@@ -15,30 +17,74 @@ class Word2Vec():
         else:
             return self.model.word_vec(word)
 
-"""
-with open("msr_paraphrase_train.txt", "r", encoding="utf-8") as f:
-    f.readline()
-    c = 0
-    cc = 0
-    for line in f:
-        sentence1 = line[:-1].split("\t")[3]
-        sentence2 = line[:-1].split("\t")[4]
-            
-        for word in nltk.word_tokenize(sentence1):
-            cc += 1
-            if word not in word2vec.vocab:
-                unknowns
+
+class MSRP():
+    def __init__(self, mode="train", batch_size=100, num_classes=2, max_length=0):
+        with open("msr_paraphrase_" + mode + ".txt", "r", encoding="utf-8") as f:
+            self.data = []
+            self.index = -1
+            self.max_length = max_length
+            self.batch_size = batch_size
+            self.num_classes = num_classes
+            self.word2vec = Word2Vec()
+
+            f.readline()
+
+            for line in f:
+                label = line[:-1].split("\t")[0]
+                s1 = nltk.word_tokenize(line[:-1].split("\t")[3])
+                s2 = nltk.word_tokenize(line[:-1].split("\t")[4])
+
+                bleu_score = nltk.translate.bleu_score.sentence_bleu(s1, s2)
+                # sentence_bleu(s1, s2, smoothing_function=nltk.translate.bleu_score.SmoothingFunction.method1)
+
+                self.data.append([s1, s2, int(label), [bleu_score]])
+                if mode == "train":
+                    self.data.append([s2, s1, int(label), [bleu_score]])
+
+                local_max_length = max(len(s1), len(s2))
+                if local_max_length > self.max_length:
+                    self.max_length = local_max_length
+
+            self.data_size = len(self.data)
+
+    def is_available(self):
+        if self.index < len(self.data):
+            return True
+        else:
+            return False
+
+    def next(self):
+        self.index += 1
+        if (self.is_available()):
+            return self.data[self.index]
+        else:
+            return [None, None, None, None]
+
+    def next_batch(self):
+        for i in range(self.batch_size):
+            s1, s2, label, feature = self.next()
+
+            if s1 is None:
+                break
+
+            # [1, d0, s]
+            s1_mat = tf.expand_dims(tf.pad(tf.stack([self.word2vec.get(w) for w in s1], axis=1),
+                                           [[0, 0], [0, self.max_length - len(s1)]]), 0)
+            s2_mat = tf.expand_dims(tf.pad(tf.stack([self.word2vec.get(w) for w in s2], axis=1),
+                                           [[0, 0], [0, self.max_length - len(s2)]]), 0)
+
+            if i == 0:
+                batch1 = s1_mat
+                batch2 = s2_mat
+                labels = [label]
+                features = [feature]
             else:
-                word2vec[word]
+                batch1 = tf.concat([s1_mat, batch1], axis=0)
+                batch2 = tf.concat([s2_mat, batch2], axis=0)
+                labels.append(label)
+                features.append(feature)
 
-
-                c += 1
-
-        for word in nltk.word_tokenize(sentence2):
-            cc += 1
-            if (word  not in word2vec.vocab):
-                #print("S2 - OOV:", word)
-                c += 1
-
-    print(c, cc, c/cc)
-"""
+        labels = tf.contrib.layers.one_hot_encoding(labels=labels, num_classes=self.num_classes)
+        features = tf.constant(features)
+        return batch1, batch2, labels, features
--- train.py
+++ train.py
@@ -1,33 +1,62 @@
-
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 
-from preprocess import Word2Vec
-import BCNN
+from preprocess import MSRP
+from BCNN import BCNN
 
-def main():
-    word2vec = Word2Vec()
 
-    s1 = "I am a boy"
-    s2 = "you are a girl"
+def main(lr=0.08, w=3, l2_reg=0.0002, batch_size=30, num_classes=2):
+    train_data = MSRP(mode="train", batch_size=batch_size, num_classes=num_classes)
+    test_data = MSRP(mode="test", batch_size=batch_size, num_classes=num_classes, max_length=train_data.max_length)
 
-    for w in s1:
-        test = word2vec.get(w)
-        print(test)
-        print(tf.shape(test))
-        break
+    print("=" * 50)
+    print("training data size:", train_data.data_size)
+    print("test data size:", test_data.data_size)
+    print("=" * 50)
 
-    #s1_mat = tf.concat([word2vec.get(w) for w in s1], 1)
-    #s2_mat = tf.concat([word2vec.get(w) for w in s2], 1)
+    model = BCNN(s=train_data.max_length, w=w, l2_reg=l2_reg, num_classes=num_classes)
+    optimizer = tf.train.AdagradOptimizer(lr).minimize(model.cost)
 
-    #print(tf.shape(s1_mat))
-    #print(tf.shapes(s2_mat))
+    # For GTX 970 memory issues
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.7)
 
-    return
-
-    model = BCNN(s=4, w=2, lr=0, l2_reg=0)
-
-    with tf.session() as sess:
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
         init = tf.global_variables_initializer()
         sess.run(init)
 
-main()
+        # trainig phase
+        i = 0
+        while train_data.is_available():
+            batch_x1, batch_x2, batch_y, batch_features = sess.run(train_data.next_batch())
+
+            _, c, train_acc = sess.run([optimizer, model.cost, model.accuracy_batch],
+                                       feed_dict={model.x1: batch_x1,
+                                                  model.x2: batch_x2,
+                                                  model.features: batch_features,
+                                                  model.y: batch_y})
+
+            i += 1
+            print("cost", i, ":", c, ", train_batch_acc:", train_acc)
+
+        print("training finished!")
+        print("=" * 50)
+
+        # test phase
+        test_acc = 0
+        while test_data.is_available():
+            batch_x1, batch_x2, batch_y, batch_features = sess.run(test_data.next_batch())
+
+            test_acc += sess.run(model.accuracy_sum,
+                                 feed_dict={model.x1: batch_x1,
+                                            model.x2: batch_x2,
+                                            model.features: batch_features,
+                                            model.y: batch_y})
+
+        test_acc = test_acc / test_data.data_size
+        print("test_acc:", test_acc)
+        print("test finished!")
+        print("=" * 50)
+
+
+main(lr=0.001, l2_reg=0.0002, batch_size=100)
