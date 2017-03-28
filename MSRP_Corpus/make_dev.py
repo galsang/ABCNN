d9b04eb102259d154594d240eab44dcb7931ebf7
galsang <kimtaeuk1000@naver.com>
2017-03-29 00:20:24 +0900

    msrp corpus modified

--- MSRP_Corpus/make_dev.py
+++ MSRP_Corpus/make_dev.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-with open("msr_paraphrase_train.txt", "r", encoding="utf-8") as f:
+with open("msr_paraphrase_train_original.txt", "r", encoding="utf-8") as f:
     attributes = f.readline()
     lines = f.readlines()
     np.random.shuffle(lines)
@@ -9,3 +9,8 @@ with open("msr_paraphrase_train.txt", "r", encoding="utf-8") as f:
         print(attributes, file=f2, end="")
         for l in lines[:400]:
             print(l, file=f2, end="")
+
+    with open("msr_paraprahse_train.txt","w", encoding="utf-8") as f3:
+        print(attributes, file=f3, end="")
+        for l in lines[400:]:
+            print(l, file=f3, end="")
