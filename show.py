0ab074d437283ba7d6dd530cc514ef30fbc1cad3
galsang <kimtaeuk1000@naver.com>
2017-04-06 02:14:07 +0900

    show.py added

---
+++ show.py
@@ -0,0 +1,19 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open("./experiments/WikiQA-ABCNN1-1-LR.txt", "r", encoding="utf-8") as f:
+    f.readline()
+    MAPs, MRRs = [], []
+
+    for line in f:
+        MAP = line[:-1].split("\t")[1]
+        MRR = line[:-1].split("\t")[2]
+
+        MAPs.append(MAP)
+        MRRs.append(MRR)
+
+plt.plot(np.arange(1, len(MAPs)+1, 1), MAPs, 'r')
+plt.plot(np.arange(1, len(MAPs)+1, 1), MRRs, 'b')
+plt.legend(["MAP", "MRR"])
+plt.show()
